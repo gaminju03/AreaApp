1cbee06d76e432e6e2015031fb994fa78d9a5baa
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-04 19:31:03 -0600

    P8AreaApp

--- AreaApp/ContentView.swift
+++ AreaApp/ContentView.swift
@@ -10,12 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+            NavigationView{
+                List{
+                    NavigationLink(destination: AreaTriangulo()){
+                        HStack{
+                            Image("triangulo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80.0, height: 100.0)
+                            Text("Área del Triangulo")
+                        }
+                    }
+                    NavigationLink(destination: AreaCirculo()){
+                        HStack{
+                            Image("circulo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80.0, height: 100.0)
+                            Text("Área del Circulo")
+                        }
+                    }
+                    NavigationLink(destination: AreaRectangulo()){
+                        HStack{
+                            Image("cuadrado")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80.0, height: 100.0)
+                            Text("Area del Cuadrado")
+                        }
+                    }
+                }.navigationBarTitle(Text("Areas APP"))
+            }
+        }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
 }
